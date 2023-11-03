8ad712290f1411caa59e50045bb8052948fc2222
pashadev <pasha.pasha12e@gmail.com>
2023-11-03 14:16:32 +0100

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO USERS (first_name, last_name, email) VALUES
+    ('John', 'Doe', 'JohnDoe@gmail.com'),
+    ('Pasha', 'Ahmad', 'PashaAhmad@gmail.com'),
+    ('Kim', 'Bar', 'KimBar@gmail.com');
