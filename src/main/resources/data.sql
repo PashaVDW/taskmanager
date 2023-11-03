6afd088b37676605d740e1e0591704a0c9418db6
pashadev <pasha.pasha12e@gmail.com>
2023-11-03 14:22:30 +0100

    commit

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO USERS (first_name, last_name, email) VALUES
-    ('John', 'Doe', 'JohnDoe@gmail.com'),
-    ('Pasha', 'Ahmad', 'PashaAhmad@gmail.com'),
-    ('Kim', 'Bar', 'KimBar@gmail.com');
